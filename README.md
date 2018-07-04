597e8a8bde912811ddcd7aff9104a4df16c22b38
Jane Liao <jane.liao@sap.com>
2018-07-04 17:01:46 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# github-md-nav
-A chrome extension to generate navigation tree for markdown on github
+# Github Markdown Navigator - Chrome Extension
+A simple Chrome extension to generate a navigation directory for markdown shown on github.
+
+:chestnut: **Example**
+![Example](images/example.png)
--- content.js
+++ content.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
+const body = document.body || document.getElementsByTagName("body")[0] || document.documentElement;
 
 ["libs/jquery.min.js", "libs/jquery-ui.min.js", "src/index.js"].forEach((file) => {
     const script = document.createElement('script')
     script.setAttribute("type", "module")
     script.setAttribute("src", chrome.extension.getURL(file))
-    head.insertBefore(script, head.lastChild);
+    body.insertBefore(script, body.lastChild);
 })
